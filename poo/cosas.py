c9c21f0a369ebdb453f57f43ce4a651f8313e8d9
ruiz ramirez casandra monserrat <72115022+casandra12@users.noreply.github.com>
2023-06-13 23:08:17 -0600

    Add files via upload

---
+++ poo/cosas.py
@@ -0,0 +1,6 @@
+class alumno:
+    facultad = "fes aragon"
+    def __init__(self, nom, ed, carr ):
+        self.__nombre = nom
+        self.__carrera = ed
+        self.__edad= carr
---
+++ poo/main.py
@@ -0,0 +1,9 @@
+
+from aritmetica import suma, resta
+def main():
+    resultado = suma(3, None)
+    print(resultado)
+    print(resta(5, 3))
+
+main()
+
