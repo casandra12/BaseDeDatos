3fa84c4abdde16ea144de56f6bc04e05e1ab3f85
ruiz ramirez casandra monserrat <72115022+casandra12@users.noreply.github.com>
2023-06-14 23:38:54 -0600

    Add files via upload

--- poo/main.py
+++ poo/main.py
@@ -1,9 +1,32 @@
+from cosas import alumno, perro
 
-from aritmetica import suma, resta
 def main():
-    resultado = suma(3, None)
-    print(resultado)
-    print(resta(5, 3))
+    al1 = alumno("monse", 19, "ico")
+    print(vars(al1))
+    al1.__nombre = "monse "
+    print(vars(al1))
+    al1.set_nombre("diego")
+    print(vars(al1))
+    print("------to string-------")
+    print(al1)
+    al1.set_edad(999)
+    print(al1)
+    al1.estudiar(5)
+    print("------perro-------")
+    perro1= perro("poddle", 2, 0.35)
+    print(vars(perro1))
+    perro1.raza = "de la calle"
+    print(vars(perro1))
+    perro1.__raza = "otra"
+    print(vars(perro1))
+    perro1.edad = 12
+    perro1.estatura = 0.43
+    print(perro1)
+    cachoro = perro.es_cachorro(perro1.edad)
+    print(cachoro)
+    perro.dormir()
+    danes = perro.perro_grande(0.8)
+    print(danes)
+
 
 main()
-
